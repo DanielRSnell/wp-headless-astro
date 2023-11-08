683a17c7ba4c178398c36749c95f4c806c14c8bb
Artisan <daniel@domartisan.com>
2023-11-08 11:55:09 -0500

    halt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import sitemap from "@astrojs/sitemap";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), svelte(), sitemap()],
+  integrations: [tailwind(), svelte(), sitemap()]
 });
---
+++ src/libs/clean.mjs
@@ -0,0 +1,48 @@
+import { parseHTML } from 'linkedom';
+
+export async function CleanMarkup(payload) {
+// Parse the HTML content with linkedom
+  const { document } = parseHTML(payload);
+
+  // Function to make a single URL relative and remove query strings
+  function makeRelative(url) {
+    let cleanUrl = url.startsWith('https://domartisan.com') ? url.replace('https://domartisan.com', '') : url;
+    return cleanUrl.split('?')[0]; // Remove query parameters
+  }
+
+  // Function to make all URLs in a srcset attribute relative and remove query strings
+  function makeSrcsetRelative(srcset) {
+    return srcset
+      .split(',')
+      .map(part => {
+        const [url, descriptor] = part.trim().split(' ');
+        return `${makeRelative(url)} ${descriptor || ''}`.trim();
+      })
+      .join(', ');
+  }
+
+  // Iterate over all elements and modify the URLs
+  const elements = document.querySelectorAll('[src], [href], [srcset]');
+  elements.forEach(el => {
+    // Replace 'src' if it's not relative and remove query strings
+    if (el.hasAttribute('src')) {
+      const src = el.getAttribute('src');
+      el.setAttribute('src', makeRelative(src));
+    }
+
+    // Replace 'href' if it's not relative and remove query strings
+    if (el.hasAttribute('href')) {
+      const href = el.getAttribute('href');
+      el.setAttribute('href', makeRelative(href));
+    }
+
+    // Replace 'srcset' if it's not relative and remove query strings
+    if (el.hasAttribute('srcset')) {
+      const srcset = el.getAttribute('srcset');
+      el.setAttribute('srcset', makeSrcsetRelative(srcset));
+    }
+  });
+
+  // Convert the document back to a string
+  return document.toString().split(import.meta.env.CORE_URL).join('').replace(/([?&]\w+=\w+)+/g, '');
+}
