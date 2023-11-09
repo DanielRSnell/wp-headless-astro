57961baeb955d56554bd3d77daf8d433150ceaca
Artisan <daniel@domartisan.com>
2023-11-09 09:57:53 -0500

    update enviroment head and footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,81 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+		scale: {
+         '10': '0.10',
+        '20': '0.20',
+        '30': '0.30',
+        '40': '0.40',
+        '50': '0.50',
+        '60': '0.60',
+        '70': '0.70',
+        '80': '0.80',
+        '90': '0.90',
+		 md: '2.25',
+      },
+      colors: {
+		gray: colors.gray,
+		slate: colors.slate,
+      },
+    },
+    fontFamily: {
+      body: [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+      sans: [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+    },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require('flowbite-typography'),
+	  require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+    require('flowbite'),
+	plugin(function({ addVariant }) {
+      addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &'])
+      addVariant('htmx-request',  ['&.htmx-request',  '.htmx-request &'])
+      addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &'])
+      addVariant('htmx-added',    ['&.htmx-added',    '.htmx-added &'])
+    }),
+  ],
+	corePlugins: {
+		preflight: true
+	}
 };
