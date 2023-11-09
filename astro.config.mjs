10268e153fc24f619a64b93cde32a01a04a09b10
Artisan <daniel@domartisan.com>
2023-11-08 20:14:19 -0500

    proxy headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
-
 import node from "@astrojs/node";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), svelte(), sitemap()]
+  integrations: [tailwind(), svelte(), sitemap(), partytown({
+    config: {
+        forward: ['dataLayer.push'],
+      },
+  })]
 });
